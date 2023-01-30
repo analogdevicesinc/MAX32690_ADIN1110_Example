9e04b688407e4f236ac230595e08bc61599d406c
lorne-maxim <65424047+lorne-maxim@users.noreply.github.com>
2023-01-30 15:42:31 -0600

    Recreated project files in "preferred" location Bumped CMSIS pack version used from 1.0.1 to 1.2.1

---
+++ examples/adin1110/lwIPExample/MAX32690/lwIPExample/IAR/RTE/RTE_Components.h
@@ -0,0 +1,19 @@
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: lwIPExample
+ * RTE configuration: lwIPExample.rteconfig
+*/
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+/*
+ * Define the Device Header File:
+*/
+#define CMSIS_device_header "max32690.h"
+
+/* Maxim.MAX32690 BSP::BSP.EvKit V1 */
+#define BOARD EvKit_V1 /* Target Board */
+
+#endif /* RTE_COMPONENTS_H */
